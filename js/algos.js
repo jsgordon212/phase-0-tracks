354dffab360b610a24a9ac3b0c25704e31e410f2
Jared Gordon <JSGordon212@gmail.com>
2016-05-09 00:34:05 -0400

    Add psuedocode to methods

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,11 @@
+/*longestWord method:
+set a variable equal to the first word or phrase of the array passed in as an argument
+loop through the array one string at a time and compare the length of the variable to 
+the next string in the array.
+IF the variable is shorter than the next string, set the variable equal to that next string.
+return the variable.*/
+
+
 function longestWord(wordList){
 
 	var longestWord = wordList[0];
@@ -13,6 +21,14 @@ function longestWord(wordList){
 	return longestWord;
 }
 
+/*commonPair method:
+set two variables equal to the array of keys returned by the Object.keys method.
+loop through the first array, setting two variables equal to the current item in the array
+and the value of that item.
+For each loop of the first array, enter a new loop of the second array.
+set two variables equal to the current item and value of the second array.
+IF the keys and the values of a given loop match return true, If not loop to the next item.
+If all the loops are completed without a matching pair, return false.*/
 
 
 function commonPair(obj1, obj2){
